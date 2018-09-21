9f3be8f15a6532135782e111b5e13a860881ac49
bouch18 <e165745@ie.u-ryukyu.ac.jp>
2018-09-21 15:24:37 +0900

    route 変更 及び 遷移追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/user'
   #root 'tops#index'
-  #get 'tops/show'
+  # get 'tops/show'
   #devise_for :users
-  get 'home/top'
+  root 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root to: "home#index"
 end
