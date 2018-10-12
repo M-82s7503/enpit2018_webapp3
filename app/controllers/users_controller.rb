5caefff9b9a0ae21a6749fb4fed8090a83a522cf
M-82s7503 <e165738@ie.u-ryukyu.ac.jp>
2018-10-12 14:21:06 +0900

    【ver0.05】 ● ユーザーページ(~/users/index.html.erb) の情報を、DBから取ってくるように改良した。 ● controller に、before_action :authenticate_user! を追加した。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  # 参考：https://qiita.com/tobita0000/items/866de191635e6d74e392
+  #  before_account 〜 で、ログイン済みのアカウントのみでしかアクセスできなくなる。
+  before_action :authenticate_user!
   def index
   end
 
