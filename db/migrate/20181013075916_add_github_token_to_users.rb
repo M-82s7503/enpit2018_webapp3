17a74360f5d5b42f076e0a29adfff7e3a55c458c
bouch18 <e165745@ie.u-ryukyu.ac.jp>
2018-10-15 20:06:36 +0900

    fix migration file

--- db/migrate/20181013075916_add_github_token_to_users.rb
+++ db/migrate/20181013075916_add_github_token_to_users.rb
@@ -1,5 +1,5 @@
 class AddGithubTokenToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :github_token, :string, null: false, after: :encrypted_password
+    add_column :users, :github_token, :string, null: true, after: :encrypted_password
   end
 end
