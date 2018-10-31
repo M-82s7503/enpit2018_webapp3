bdda79fc5cccf60819d8f59760bc0b8f4e21dd09
M-.82s7503 <e165738@ie.u-ryukyu.ac.jp>
2018-11-01 01:20:20 +0900

    ● 構想中。途中経過commit

--- app/mailers/yagi_no_tegami_mailer.rb
+++ app/mailers/yagi_no_tegami_mailer.rb
@@ -1,27 +1,22 @@
 class YagiNoTegamiMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.yagi_no_tegami_mailer.regular_mail.subject
-  #
   def regular_mail(user, project)
     @user = user
     @project = project
     # コミット数：0 になったプロジェクトに、メールを送る。
     if @project.commit_num == 0
         puts("          →  メール送信 (0なので)\n\n")
-        @attach_name = 'ゴートの叫び.png'
+        @attach_name = select_random_mail_image(0)
         attachments.inline[@attach_name] = File.read("#{Rails.root}/app/assets/images/yagis/#{@attach_name}")
         mail(to: @user.email, from:'from@example.com', subject: "【HAS】Project : #{@project.name} のヤギから手紙が届きました！")
     end
   end
+  # mail_pattarn に従って、画像をランダムに選ぶ
+  def select_random_mail_image(mail_pattarn)
+    @img_list_table = CSV.table('mail_image_lists.csv')
+  end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.yagi_no_tegami_mailer.special_mail.subject
-  #
+
   def special_mail
     @greeting = "Hi"
 
