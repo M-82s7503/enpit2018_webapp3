0a9d5f573b7342fd7e4a4f02a698e23cb8f08f60
e165715 <e165715@ie.u-ryukyu.ac.jp>
2018-11-19 18:13:42 +0900

    login button 変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_11_11_060348) do
-
 
   create_table "github_commit_logs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "users_id"
